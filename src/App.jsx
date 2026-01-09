7e5c1dc29a2026082388bf5a1eba1ac7c0851335
Aman Jainwal <jainwalaman@gmail.com>
2026-01-09 21:48:43 +0530

    import navbar in app file

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { lazy, Suspense } from "react";
 import { Routes, Route } from "react-router";
-
+import Navbar from "./components/Navbar"
 const BookDetailsPage = lazy(() => import("./pages/BookDetailsPage"));
 const SearchPage = lazy(() => import("./pages/SearchPage"));
 const FavoritesPage = lazy(() => import("./pages/FavoritesPage"));
@@ -8,6 +8,7 @@ const FavoritesPage = lazy(() => import("./pages/FavoritesPage"));
 function App() {
   return (
     <>
+    <Navbar/>
       <Suspense fallback={<p>Loading...</p>}>
         <Routes>
           <Route path="/" element={<SearchPage />} />
