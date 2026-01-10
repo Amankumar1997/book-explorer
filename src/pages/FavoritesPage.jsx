359380b92db31e09d775b36b059df9dcfa84da3d
Aman Jainwal <jainwalaman@gmail.com>
2026-01-10 11:08:09 +0530

    show favorites list data

--- src/pages/FavoritesPage.jsx
+++ src/pages/FavoritesPage.jsx
@@ -1,6 +1,17 @@
+import { useSelector } from "react-redux";
+import BookCard from "../components/BookCard";
 
 const FavoritesPage = () => {
-  return <div>Fav Page</div>;
+  const favorites = useSelector((state) => state.favorites);
+
+  return (
+    <div>
+      <h2>Favorites</h2>
+      {favorites.map((book) => (
+        <BookCard key={book.id} book={book} isFavorite />
+      ))}
+    </div>
+  );
 };
 
 export default FavoritesPage;
