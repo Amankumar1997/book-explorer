9f12777851f8f5897ac9d1c9cf9b5dfb6546b3ff
Aman Jainwal <jainwalaman@gmail.com>
2026-01-10 21:10:26 +0530

    search page css issue fixed

--- src/pages/SearchPage.jsx
+++ src/pages/SearchPage.jsx
@@ -7,33 +7,35 @@ const SearchPage = () => {
   const { data, status, error } = useSelector((state) => state.books.books);
   const favorites = useSelector((state) => state.favorites.favorites);
 
-
-  {
-    status === STATUS.LOADING && <p className="loading-text">Loading...</p>;
-  }
-  {
-    error ? <div>{error}</div> : <></>;
-  }
   return (
     <div class="page-wrapper">
       <div className="search-page">
         <div className="search-form-wrapper">
           <SearchForm />
         </div>
+        {error && <div className="error-text">{error}</div>}
 
-        {data && data.length ? (
-          <div className="books-grid">
-            {data?.map((book) => (
-              <BookCard
-                key={book.id}
-                book={book}
-                isFavorite={favorites.some((fav) => fav.id === book.id)}
-              />
-            ))}
+        {status === STATUS.LOADING ? (
+          <div className="page-loader">
+            <p className="loading-text">Loading...</p>
           </div>
         ) : (
           <>
-            <div>No data found</div>
+            {data && data.length ? (
+              <div className="books-grid">
+                {data?.map((book) => (
+                  <BookCard
+                    key={book.id}
+                    book={book}
+                    isFavorite={favorites.some((fav) => fav.id === book.id)}
+                  />
+                ))}
+              </div>
+            ) : (
+              <>
+                <div>No data found</div>
+              </>
+            )}
           </>
         )}
       </div>
